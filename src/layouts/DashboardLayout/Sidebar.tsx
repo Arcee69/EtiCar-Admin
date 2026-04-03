38ee7f4c79b4ebf4847a9e395eacadbfe72ef31f
Arcee69 <olawalearcee@gmail.com>
2026-04-03 11:38:22 +0100

    Chore: Hot Fix

--- src/layouts/DashboardLayout/Sidebar.tsx
+++ src/layouts/DashboardLayout/Sidebar.tsx
@@ -87,7 +87,7 @@ const Sidebar = ({ isOpen, onClose }: SidebarProps) => {
       </div>
 
       {/* Navigation */}
-      <nav className="flex-1 overflow-y-hidden py-4  px-3">
+      <nav className="flex-1 overflow-y-auto py-4  px-3">
         {sections.map((section) => (
           <div key={section.label} className="mb-4">
             <p className="px-3 mb-2 text-xs font-semibold text-GREY-200 tracking-widest uppercase">
